0d167baf68577e64b41f983717d115c4c5541f29
Hasan BASBUNAR <basbunarhasan@gmail.com>
2023-10-30 22:11:23 +0100

    zkndze

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
-var server = express();
-server.use(express.static(__dirname + '/public'));
-
-server.get('*', function(req, res){
-  res.sendFile('index.html');
-});
-
-//app.use(express.static('public'));
-
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
-//const port = 3000;
-//const dotenv = require('dotenv').config();
 
 let connectionStatus = "Connexion en cours...";
 let connectionColor = "green"; // Couleur initiale en vert
 let db;
 
-// Configuration de la base de donnéess
+// Configuration de la base de données
 const dbConfig = {
-  host: "db4free.net",//process.env.DB_HOST,
+  host: "db4free.net",
   user: 'hasanbb',
   password: 'hASANIBASRI57µ',
-  database: "datavizu"//'basbunar2u_denverCrimes'
+  database: "datavizu"
 };
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Bienvenue');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Fonction pour mettre à jour le statut de connexion
@@ -41,7 +31,7 @@ function updateConnectionStatus(status, color) {
 // Fonction pour tenter de se connecter à la base de données
 function tryDatabaseConnection() {
   db = mysql.createConnection(dbConfig);
-  
+
   db.connect((err) => {
     if (err) {
       updateConnectionStatus('Erreur de connexion à la base de données : ' + err.message, 'red');
@@ -62,29 +52,24 @@ tryDatabaseConnection();
 // Utilisez bodyParser pour analyser les données JSON
 app.use(bodyParser.json());
 
-// Mettez à jour la route pour gérer la requête POST
 app.post('/donnees', (req, res) => {
-  const query = req.body.query; // Obtenez la requête SQL du corps de la demande
+  const query = req.body.query;
 
   db.query(query, (err, results) => {
     if (err) {
       console.error('Erreur lors de la récupération des données :', err);
-      res.status(100000).send('Erreur serveur');
+      res.status(500).send('Erreur serveur');
       return;
     }
     res.json(results);
   });
 });
 
-var port = 10001;
-server.listen(port, function() {
-    console.log('server listening on port ' + port);
-});
+const port = process.env.PORT || 10001;
 
-// Démarrer le serveur
-//app.listen(port, () => {
-//  console.log(`Serveur Node.js en cours d'exécution sur le port ${port}`);
-//});
+app.listen(port, () => {
+  console.log(`Serveur Node.js en cours d'exécution sur le port ${port}`);
+});
 
 // Exposez l'état de la connexion à un point de terminaison
 app.get('/connection-status', (req, res) => {
