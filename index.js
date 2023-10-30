df727a4b3342f11fbb7b7522603ab9c6cf93f010
Hasan BASBUNAR <basbunarhasan@gmail.com>
2023-10-30 21:51:09 +0100

    dkenfk

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const dbConfig = {
 };
 
 app.get('/', (req, res) => {
-  res.redirect('./public/index.html');
+  res.redirect('./index.html');
 });
 
 // Fonction pour mettre à jour le statut de connexion
